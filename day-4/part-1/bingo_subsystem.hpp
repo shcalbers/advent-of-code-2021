a6696c5b46b6e86ac1ceab9ed763934b5cd307f1
shcalbers <34623185+shcalbers@users.noreply.github.com>
2021-12-04 16:18:11 +0100

    Solve Day 4 Part 1

---
+++ day-4/part-1/bingo_subsystem.hpp
@@ -0,0 +1,99 @@
+#ifndef ADVENT_OF_CODE__BINGO_SUBSYSTEM
+#define ADVENT_OF_CODE__BINGO_SUBSYSTEM
+
+#include <cassert>
+
+#include <fstream>
+#include <iostream>
+
+#include <vector>
+#include <string>
+#include <vector>
+
+#include <limits>
+
+#include "bingo_board.hpp"
+
+namespace advent_of_code {
+
+inline namespace day4 {
+
+class BingoSubsystem final {
+
+public:
+
+    ~BingoSubsystem(void) noexcept = delete;
+
+    static auto run(std::string file_name) noexcept -> void;
+
+private:
+
+    static auto parse(std::string file_name, std::vector<int>& numbers_out, std::vector<BingoBoard>& boards_out) noexcept -> void;
+
+};
+
+inline auto BingoSubsystem::run(std::string file_name) noexcept -> void {
+    std::vector<int> numbers;
+    std::vector<BingoBoard> boards;
+
+    numbers.reserve(100);
+    boards.reserve(100);
+
+    parse(file_name, numbers, boards);
+
+    for (auto number : numbers) {
+        std::cout << "Calling number... " << number << "!\n";
+
+        for (auto& board : boards) {
+            board.mark(number);
+            if (board.has_bingo()) {
+                std::cout << "BINGO! The following board won:\n" << board << '\n';
+                std::cout << "Total Score: " << board.score() << '\n';
+                return;
+            }
+        }
+    }
+
+    std::cout << "Nobody won this round... :c" << std::endl;
+    return;
+}
+
+inline auto BingoSubsystem::parse(std::string file_name, std::vector<int>& numbers_out, std::vector<BingoBoard>& boards_out) noexcept -> void {
+    static constexpr auto board_size = 5;
+
+    auto input = std::ifstream(file_name);
+
+    // Read numbers to draw
+    {
+        int number = 0;
+        char delimiter = '\0';
+        while (input >> number >> delimiter) {
+            numbers_out.push_back(number);
+            if (delimiter != ',') break;
+        }
+    }
+
+    // Read Bingo Boards
+    while (input.peek() != EOF) {
+        std::vector<std::vector<int>> numbers(board_size, std::vector<int>(board_size));
+        for (auto row = 0; row < board_size; row++) {
+            for (auto col = 0; col < board_size; col++) {
+                input >> numbers[row][col];
+            }
+        }
+
+        boards_out.emplace_back(numbers);
+        
+        // Skip blank lines
+        input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (input.peek() == EOF) break;
+    }
+
+    return;
+}
+
+}
+
+}
+
+#endif
