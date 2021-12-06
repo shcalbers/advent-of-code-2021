79c7ba81468d9bbfa6842dd78bcd3739a02b4df0
shcalbers <34623185+shcalbers@users.noreply.github.com>
2021-12-06 14:51:09 +0100

    Solve Day 6 Part 1

---
+++ day-6/part-1/solution.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+auto main(void) noexcept -> int {
+    static constexpr auto simulation_length = 80;
+
+    static constexpr auto newborns = 8;
+    static constexpr auto pregnant = 6;
+    static constexpr auto in_labor = 0;
+    static constexpr auto total_generations = 9;
+    std::vector<size_t> lanternfishies(total_generations);
+
+    {
+        auto input = std::ifstream("input.txt");
+        for (auto generation = 0; input >> generation; input.ignore()) {
+            lanternfishies[generation]++;
+        }
+    }
+
+    for (auto day = 0; day < simulation_length; day++) {
+        auto birthing_fishies = lanternfishies[in_labor];
+        for (auto generation = 1; generation < lanternfishies.size(); generation++) {
+            lanternfishies[generation-1] = lanternfishies[generation]; 
+        }
+
+        lanternfishies[pregnant] += birthing_fishies;
+        lanternfishies[newborns]  = birthing_fishies;
+    }
+
+    size_t total_fishies = 0;
+    for (auto fishies : lanternfishies) total_fishies += fishies;
+
+    std::cout << "After running a simulation of " << simulation_length << " days there should be a total of " << total_fishies << " lanternfishies." << std::endl;
+
+    return 0;
+}
