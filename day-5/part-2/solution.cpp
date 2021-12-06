6c48fc438b3d0ce732d7c189c7ce398070a9b517
shcalbers <34623185+shcalbers@users.noreply.github.com>
2021-12-06 16:56:10 +0100

    Solve Day 5 Part 2

---
+++ day-5/part-2/solution.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "htvent_subsystem.hpp"
+
+using namespace advent_of_code::day_5;
+
+auto main(void) -> int {
+    HTVentSubsystem::run("input.txt");
+    return 0;
+}
